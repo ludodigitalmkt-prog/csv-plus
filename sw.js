c43a8a6a6dd5862477a035875a640a2b134d0bd5
ludodigitalmkt-prog <ludodigitalmkt@gmail.com>
2026-03-04 11:43:32 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,21 @@
-const CACHE_NAME = 'csv-plus-v1';
+const CACHE_NAME = 'csv-plus-v2';
 const urlsToCache = [
-  './index.html',
-  './manifest.json',
-  './logo.png'
+  '.',
+  'index.html',
+  'manifest.json',
+  'logo.png'
 ];
 
-// Instala o motor offline e guarda os arquivos
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Se estiver sem internet, ele busca do Cache
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) return response;
-        return fetch(event.request);
-      })
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
